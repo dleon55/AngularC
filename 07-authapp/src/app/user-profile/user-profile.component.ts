d7c2ef95c8ab45ed918edef933e2b564b153fc1b
David León Gómez <dleon55@hotmail.com>
2021-03-06 23:06:10 -0600

    perfil de usuario completo

--- 07-authapp/src/app/user-profile/user-profile.component.ts
+++ 07-authapp/src/app/user-profile/user-profile.component.ts
@@ -4,11 +4,27 @@ import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-user-profile',
   template: `
-    <div *ngIf="auth.user$ | async as user" class="card" style="width: 18rem;">
-      <!-- <img src="..." class="card-img-top" alt="..." /> -->
-      <div class="card-body">
-        <h5 class="card-title">{{ user.name }}</h5>
-        <p class="card-text">{{ user.email }}</p>
+    <div class="container">
+      <div
+        *ngIf="auth.user$ | async as user"
+        class="card"
+        style="width: 18rem;"
+      >
+        <img src="{{ user.picture }}" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{{ user.name }}</h5>
+          <p class="card-text">{{ user.email }}</p>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">{{ user.nickname }}</li>
+            <li class="list-group-item">
+              Verificado: {{ user.email_verified }}
+            </li>
+            <li class="list-group-item">{{ user.sub }}</li>
+          </ul>
+          <p class="card-text">
+            <small class="text-muted">{{ user.updated_at }}</small>
+          </p>
+        </div>
       </div>
     </div>
   `,
