92b7ae9e1fcbfadff79a19e90207a7b3c9e3d13d
David León Gómez <dleon55@hotmail.com>
2021-04-02 23:37:06 -0600

    rutas paises

---
+++ c-pasisesApp/src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent,
+    MenuComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    MenuComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
