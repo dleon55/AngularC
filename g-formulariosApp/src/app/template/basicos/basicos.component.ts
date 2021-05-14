7d400689a7b02c05a74013c73680c6b46d16a3df
David León Gómez <dleon55@hotmail.com>
2021-05-13 19:45:04 -0500

    ajustes de no empleado

--- g-formulariosApp/src/app/template/basicos/basicos.component.ts
+++ g-formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -12,6 +12,9 @@ export class BasicosComponent implements OnInit {
     producto: '',
     precio: 0,
     existencias: 0,
+    noEmpleado:0,
+    anio:2020,
+    quincena:1
   };
   quincenas :any[]=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
   constructor() {}
